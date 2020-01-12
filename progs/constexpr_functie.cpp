fcc9639003ff309b53963af60175103566a11cd4
HarryBroeders <j.z.m.broeders@hr.nl>
2020-01-12 13:51:49 +0100

    WIP (met fout)

--- progs/constexpr_functie.cpp
+++ progs/constexpr_functie.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
+#include <array>
 using namespace std;
 
-constexpr unsigned int cpow(unsigned int n, unsigned int m) {
+unsigned int power(unsigned int n, unsigned int m) {
     unsigned int result = 1;
     for (unsigned int i {0}; i < m; ++i) {
         result *= n;
@@ -9,16 +10,12 @@ constexpr unsigned int cpow(unsigned int n, unsigned int m) {
     return result;
 }
 
-constexpr unsigned int cpowr(unsigned int n, unsigned int m) {
-    return m == 0 ? 1 : n * cpowr(n, m - 1);
-}
-
 int main() {
-    constexpr unsigned int c1 = cpow(3, 5);
-    cout << "3^5 = " << c1 << '\n';
+    cout << "3 tot de macht 5 = " << power(3, 5) << '\n';
 
-    constexpr unsigned int c2 = cpowr(3, 5);
-    cout << "3^5 = " << c2 << '\n';
+    array<int, power(3, 5)>;
+    constexpr unsigned int c1 = power(3, 5);
+    cout << "3 tot de macht 5 = " << c1 << '\n';
 
     unsigned int a, b;
     cout << "Geef een integer: ";
@@ -26,9 +23,5 @@ int main() {
     cout << "Geef nog een integer: ";
     cin >> b;
     
-//  constexpr unsigned int c3 = cpow(a, b);
-//  Error: the value of `a' is not usable in a constant expression
-    
-    cout << "a^b = " << cpow(a, b) << '\n';
-    cout << "a^b = " << cpowr(a, b) << '\n';
+    cout << a << " tot de macht " << b << " = " << power(a, b) << '\n';
 }
