f4db41965e6f381839c45ed72f6673f7f75e8f10
Harry Broeders <J.Z.M.Broeders@hr.nl>
2026-03-06 19:38:39 +0100

    print i.p.v. cout

--- progs/formatSingleValue.cpp
+++ progs/formatSingleValue.cpp
@@ -1,42 +1,37 @@
 // formatSingleValue.cpp
 // https://www.modernescpp.com/index.php/formatting-user-defined-types-in-c20/
+// BroJZ aangepast aan C++26
 
-#include <format>
-#include <iostream>
+import std;
+using namespace std;
 
-class SingleValue {                                          // (1)
- public: 
-   SingleValue() = default;
-   explicit SingleValue(int s): singleValue{s} {}
-   int getValue() const {                                    // (2)
-     return singleValue;
-   }
- private:
-   int singleValue{};
+class SingleValue {
+public: 
+  SingleValue() = default;
+  explicit SingleValue(int s): singleValue{s} {}
+  int getValue() const {
+    return singleValue;
+  }
+private:
+  int singleValue{};
 };
 
-template<>                                                   // (3)
+template<>
 struct std::formatter<SingleValue> {
-  constexpr auto parse(std::format_parse_context& context) { // (4)
+  constexpr auto parse(format_parse_context& context) {
     return context.begin();
   }
-  auto format(const SingleValue& sVal, std::format_context& context) const {  // (5)
+  auto format(const SingleValue& sVal, format_context& context) const {
     return std::format_to(context.out(), "{}", sVal.getValue());
   }
 };
 
 int main() {
-
-  std::cout << '\n'; 
-
   SingleValue sVal0;
   SingleValue sVal2020{2020};
   SingleValue sVal2023{2023};
 
-  std::cout << std::format("Single Value: {} {} {}\n", sVal0, sVal2020, sVal2023);
-  std::cout << std::format("Single Value: {1} {1} {1}\n", sVal0, sVal2020, sVal2023);
-  std::cout << std::format("Single Value: {2} {1} {0}\n", sVal0, sVal2020, sVal2023);
-
-  std::cout << '\n';
-
+  println("Single Value: {} {} {}", sVal0, sVal2020, sVal2023);
+  println("Single Value: {1} {1} {1}", sVal0, sVal2020, sVal2023);
+  println("Single Value: {2} {1} {0}", sVal0, sVal2020, sVal2023);
 }
