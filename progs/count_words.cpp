9dafefec07e7bb0157e7bc521574cad22833efdb
Harry Broeders <J.Z.M.Broeders@hr.nl>
2026-04-22 19:50:17 +0200

    WIP naar C++23

--- progs/count_words.cpp
+++ progs/count_words.cpp
@@ -4,13 +4,13 @@ using namespace std;
 int main() {
     string w;
     map<string, int> freq;
-    println("Geef filenaam: ");
+    print("Geef filenaam: ");
     cin >> w;
     ifstream fin {w};
     while (fin >> w) {
         ++freq[w];
     }
-    println("woordfrequentie: {}", freq);
+    println("Woordfrequentie: {}", freq);
     println("Enkele belangrijke keywords:");
     println("if: {}", freq["if"]);
     println("else: {}", freq["else"]);
--- progs/temp.cpp
+++ progs/temp.cpp
@@ -1,20 +1,5 @@
 import std;
 using namespace std;
 
-template<typename T> 
-struct std::formatter<complex<T>>: public formatter<T> {
-    auto format(const complex<T>& z, auto& context) const {
-        context.advance_to(formatter<T>::format(z.real(), context));
-        if (z.imag() >= 0)
-            context.advance_to(format_to(context.out(), "+"));
-        context.advance_to(formatter<T>::format(z.imag(), context));
-        context.advance_to(format_to(context.out(), "j"));
-        return context.out();
-    }
-};
-
 int main() {
-    complex<double> z {1.2, -2.9};
-    cout << "z = " << z << '\n';
-    println("z = {:.3f}", z);
 }
